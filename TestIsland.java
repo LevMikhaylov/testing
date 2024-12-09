541fdb22ea8c0cf1d802897408d18df2dc52492b
LevMikhaylov <152269424+LevMikhaylov@users.noreply.github.com>
2024-12-09 17:13:01 +0300

    Update TestIsland.java

--- TestIsland.java
+++ TestIsland.java
@@ -114,4 +114,33 @@ public class TestIsland {
             System.out.println(e.getMessage());
         }
     }
+    @Test
+    public void testmove(){
+        Island island = new Island(100,20);
+        Wolf wolf1 = new Wolf("Wolf1", 5, 3, 45, 0, 0);
+        Wolf wolf2 = new Wolf("Wolf2", 5, 4, 45, 0, 0);
+        Rabbit rabbit1 = new Rabbit("Rabbit1", 5, 45, 45, 1, 0);
+        Rabbit rabbit2 = new Rabbit("Rabbit2", 5, 45, 45, 0, 1);
+        Rabbit rabbit3 = new Rabbit("Rabbit3", 5, 45, 45, 1, 0);
+        Fox fox = new Fox("Fox1", 5, 45, 45, 0, 2);
+        Goat goat1 = new Goat("Goat",5,32,12,6,7);
+        Hog hog1 = new Hog("Hog",4,12,4,5,5);
+        Mouse mouse1 = new Mouse("Mouse",3,12,34,0,2);
+        Horse horse1 = new Horse("Horse",4,3,12,0,2);
+        Horse horse2 = new Horse("Horse",4,3,12,0,2);
+        Island.addAnimalToCell(wolf1, 0, 0);
+        Island.addAnimalToCell(wolf2, 0, 0);
+        Island.addAnimalToCell(fox, 0, 0);
+        Island.addAnimalToCell(rabbit1, 0, 0);
+        Island.addAnimalToCell(rabbit2, 0, 0);
+        Island.addAnimalToCell(rabbit3, 0, 0);
+        Island.addAnimalToCell(goat1,0,0);
+        Island.addAnimalToCell(hog1,0,0);
+        Island.addAnimalToCell(mouse1,0,0);
+        Island.addAnimalToCell(horse1,1,1);
+        Island.addAnimalToCell(horse2,3,1);
+        horse2.move(horse2,1,2);
+        mouse1.move(mouse1,2,3);
+        Assert.assertNotNull(island.getCell(5,5).getAnimals());
+}
 }
